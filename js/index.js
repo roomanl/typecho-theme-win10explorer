0364b97ccdfcf2979a41f22f13a3b9fe096b8989
roomanl <roomanl@163.com>
2020-06-01 16:58:08 +0800

    。

--- js/index.js
+++ js/index.js
@@ -75,6 +75,7 @@ $(function(){
             initWin10Height();
         }
     }
+    console.log(" %c \u5c71\u9876\u6d1e\u6d1e\u4eba %c \u0054\u0068\u0065\u006d\u0065\u0020\u0062\u0079\u0020\u5c71\u9876\u6d1e\u6d1e\u4eba\u002e\u0020\u007c\u0020\u0068\u0074\u0074\u0070\u003a\u002f\u002f\u0072\u006f\u006f\u0074\u0076\u0069\u0070\u002e\u0063\u006e", "color: #FFFFFF; background: #FF4081; padding:6px;", "color: #FFFFFF; background: #424242; padding:6px;");
 });
 $(window).resize(function (){
     initWin10Height();
